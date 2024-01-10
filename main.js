ba11c37f85787c317ecc78fa47670a3c5c2189e5
calhounbryce13 <calhounbryce13@gmail.com>
2024-01-10 14:14:14 -0800

    added animations

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const videoSpeed = 0.5;
 
 document.addEventListener('DOMContentLoaded', function() {
     let Introvideo = document.getElementsByClassName("myVideo")[0];
@@ -7,12 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     BackgroundVideo.addEventListener('loadeddata', function() {
-        BackgroundVideo.playbackRate = 0.5;
+        BackgroundVideo.playbackRate = videoSpeed;
     });
 
 
     Introvideo.addEventListener("ended", function(){
         this.classList.add("hide-video");
+
+        setTimeout(function(){
+
+            // removing the video from the DOM
+            Introvideo.parentNode.removeChild(Introvideo);
+        }, 3000);
+        
+        
 
     });
     
