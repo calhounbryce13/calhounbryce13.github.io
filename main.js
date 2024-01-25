60b508f4f818dc86a5e77523336762f312d922c4
calhounbryce13 <calhounbryce13@gmail.com>
2024-01-25 00:58:41 -0800

    updates to projects

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
     let BackgroundVideo = document.getElementById("background-video");
 
 
-    BackgroundVideo.addEventListener('loadeddata', function() {
+    BackgroundVideo.addEventListener('loadeddata', () => {
         BackgroundVideo.playbackRate = videoSpeed;
     });
 
 
-    Introvideo.addEventListener("ended", function(){
+    Introvideo.addEventListener("ended", function() {
         this.classList.add("hide-video");
 
-        setTimeout(function(){
+        setTimeout(() => {
 
             // removing the video from the DOM
             Introvideo.parentNode.removeChild(Introvideo);
