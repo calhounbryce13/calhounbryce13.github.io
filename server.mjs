2bd7bc1e703e2522581157690ec90e13aef7d539
calhounbryce13 <calhounbryce13@gmail.com>
2024-03-08 19:35:48 -0800

    updates

--- server.mjs
+++ server.mjs
@@ -3,6 +3,15 @@ import express from 'express';
 const app = express();
 const PORT = 3000
 import * as users from './page_model.mjs'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+//app.use(express.static('public'));
+
+const __filename = fileURLToPath(import.meta.url);
+const directory = dirname(__filename);
+
+app.use(express.static(directory));
 
 
 app.get('/contact', async (req, res) => {
