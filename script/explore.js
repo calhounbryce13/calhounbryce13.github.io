aea3df47250c4e1c1606e9d6552af8798b0879cd
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-12-22 01:51:15 -0800

    more maze updates

--- script/explore.js
+++ script/explore.js
@@ -1,17 +1,46 @@
 'use strict';
 
 const MAX = 20
-const SIZE = 6
+const SIZE = 10
 
 const WALLS = [
+    [8, 0],
+    [7, 0],
+    [6, 0],    
+    [5, 0],
+    [4, 9],
+    [5, 9],
+    [6, 9],    
+    [7, 9],
+    [6, 3],
+    [6, 4],
+    [6, 5],    
+    [6, 6],
+    [0, 8],
+    [0, 9],
+    [1, 6],
+    [2, 6],
+    [8, 5],
+    [9, 5],
+    [8, 7],
+    [9, 7],
+    [9, 8],
+    [2, 8],
+    [4, 7],
+    [5, 7],
+    [8, 3],
+    [8, 2],
+    [9, 3],
+    [9, 2],
     [0, 0],
     [0, 1],
-    [0, 3],
+    [0, 3],    
     [1, 4],
+    [2, 0],
     [2, 2],
     [3, 3],
     [4, 3],
-
+    [4, 4],
     [4, 5],
     [4, 1],
     [5, 1],
@@ -111,6 +140,7 @@ const tap_goal_functionality = function(){
             const cell = rows[x].children[y];
             cell.addEventListener('click', (event) => {
                 if(!(event.target.classList.contains('wall'))){
+                    console.log("\n ", x, y);
                     if(!(event.target.classList.contains('start'))){
                         if(!(localStorage.getItem('goal'))){
                             event.target.classList.add('goal');
@@ -169,18 +199,21 @@ const valid_neighbor = function(given){
 
 const get_neighbors = function(current){
     const neighbors = [];
+
+
     if(valid_neighbor([current[0] - 1, current[1]])){   // up
         neighbors.push([current[0] - 1, current[1]])
-    }
-    if(valid_neighbor([current[0] + 1, current[1]])){   // down
-        neighbors.push([current[0] + 1, current[1]])
     }
     if(valid_neighbor([current[0], current[1] - 1])){   // left
         neighbors.push([current[0], current[1] - 1])
     }
+    if(valid_neighbor([current[0] + 1, current[1]])){   // down
+        neighbors.push([current[0] + 1, current[1]])
+    }
     if(valid_neighbor([current[0], current[1] + 1])){   // right
         neighbors.push([current[0], current[1] + 1])
     }
+
     return neighbors;
 }
 
