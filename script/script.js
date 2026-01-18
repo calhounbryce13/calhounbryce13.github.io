7984377b22cf400c7d232f4f998a91c19cb45201
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2026-01-18 01:33:35 -0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -22,9 +22,9 @@ const primordial_ooze = function(){
     const oozes = document.querySelectorAll('.ooze');
     oozes.forEach((ooze, i) => {
         const duration = 60 + Math.random() * 60;  
-        const delay = -Math.random() * duration;    // staggered
-        const x = 10 + Math.random() * 80;           // 10%–90%
-        const size = 30 + Math.random() * 80;        // 20px–100px
+        const delay = -Math.random() * duration;
+        const x = Math.random() * 95;
+        const size = 30 + Math.random() * 80;
 
         ooze.style.setProperty('--duration', `${duration}s`);
         ooze.style.setProperty('--delay', `${delay}s`);
